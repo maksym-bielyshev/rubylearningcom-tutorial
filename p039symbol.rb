a75be3f042d40275d7f431f86d1d616103238a9e
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-08-25 21:12:49 +0300

    Add symbols example

---
+++ p039symbol.rb
@@ -0,0 +1,6 @@
+# use the object_id method of class Object
+# it returns an integer identifier for an object
+puts "string".object_id
+puts "string".object_id
+puts :symbol.object_id
+puts :symbol.object_id
