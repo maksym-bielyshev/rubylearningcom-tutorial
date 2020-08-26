af954ce63f77e3cf21c27f40215d19ca1f504c6c
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-08-26 22:04:23 +0300

    Add an access violation example

---
+++ p047classaccess.rb
@@ -0,0 +1,14 @@
+class ClassAccess
+  def m1          # this method is public
+  end
+  protected
+  def m2        # this method is protected
+  end
+  private
+  def m3        # this method is private
+  end
+end
+ca = ClassAccess.new
+ca.m1
+#ca.m2
+#ca.m3
